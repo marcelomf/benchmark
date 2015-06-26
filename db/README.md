10c047e4342921ae46233342d50b24bde2028866
Marcelo M. Fleury <marcelomf@gmail.com>
2015-06-26 12:12:31 -0300

    Benchmarks rox!

--- db/README.md
+++ db/README.md
@@ -1,12 +1,50 @@
-### Databases Benchmark
+## Databases Benchmark
+
+### Supported Databases
+- postgres -> OK
+- mysql -> OK
+- mariadb -> OK
+- mongodb -> OK
+- elasticsearch -> OK
+- sqlite -> OK
+- couchdb -> Docker ok, but need nodejs code 
+- riak-> Docker ok, but need nodejs code
+- orientdb -> Docker ok, but need nodejs code
+- couchbase -> Docker ok, but need nodejs code
+- cassandra -> Docker ok, but need nodejs code
+- solr -> Docker ok, but need nodejs code
+- hadoop -> Docker ok, but need nodejs code
+- arangodb -> Not ok
+- scaledb -> Not ok
+- iotdb -> Not ok
+- memsql -> Not ok
+- leveldb -> Not ok
+- simpledb -> Not ok
+- Research bigtable
+- Research dremel BigQuery google
+- Research influxdb
+- Research fluentd
+- Research logstash / splunk
+
+### First steps
 Clone repo and entry dir:
+
 `git clone https://github.com/marcelomf/benchmark`
+
 'cd benchmark/db'
+
 Create docker containers:
-`./build_env.sh`
+
+`sudo ./build_env.sh`
+
 Create your uri.txt file with password of databases(see the password with docker logs command):
+
 `vim|nano|joe uri.txt`
+
 Run your benchmark:
-`./benchmark.sh [xml|csv] [directory|file] > benchmark.log 2>&1`
+
+`sudo ./benchmark.sh [xml|csv] [directory|file] > benchmark.log 2>&1`
+
 Grep your log and generate statistics csv:
+
 `./grep_log.sh benchmark.log`
--- db/build_env.sh
+++ db/build_env.sh
@@ -12,43 +12,43 @@ sudo npm install -g bookshelf caminte elasticsearch mongodb mongoose mysql orm p
 
 db="bench"
 
-docker run -d -p 5432:5432 --name postgres tutum/postgresql
-docker logs postgres_5432_$bench | head -n 30
+docker run -d -p 5432:5432 --name postgres_5432_$db tutum/postgresql
+docker logs postgres_5432_$db | head -n 30
 
-docker run -d -p 3306:3306 --name mysql_3306_$bench tutum/mysql
-docker logs mysql_3306_$bench | head -n 30
+docker run -d -p 3306:3306 --name mysql_3306_$db tutum/mysql
+docker logs mysql_3306_$db | head -n 30
 
-docker run -d -p 3366:3306 --name mariadb_3366_$bench tutum/mariadb
-docker logs mariadb_3366_$bench | head -n 30
+docker run -d -p 3366:3306 --name mariadb_3366_$db tutum/mariadb
+docker logs mariadb_3366_$db | head -n 30
 
-docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no --name mongodb_27017_$bench tutum/mongodb
-docker logs mongodb_27017_$bench | grep "mongo admin"
+docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no --name mongodb_27017_$db tutum/mongodb
+docker logs mongodb_27017_$db | grep "mongo admin"
 
-docker run -d -p 9200:9200 --name elastic_9200_$bench tutum/elasticsearch
-docker logs elastic_9200_$bench | head -n 30
+docker run -d -p 9200:9200 --name elastic_9200_$db tutum/elasticsearch
+docker logs elastic_9200_$db | head -n 30
 
-docker run -d -p 5984:5984 --name couchdb_5984_$bench tutum/couchdb
-docker logs couchdb_5984_$bench | head -n 30
+docker run -d -p 5984:5984 --name couchdb_5984_$db tutum/couchdb
+docker logs couchdb_5984_$db | head -n 30
 
-docker run -d -p 8087:8087 -p 8098:8098 --name riak_8087_$bench tutum/riak
-docker logs riak_8087_$bench | head -n 30
+docker run -d -p 8087:8087 -p 8098:8098 --name riak_8087_$db tutum/riak
+docker logs riak_8087_$db | head -n 30
 
-docker run -d -p 2424:2424 -p 2480:2480 --name orientdb_2424_$bench joaodubas/orientdb
-docker logs orientdb_2424_$bench | head -n 30
+docker run -d -p 2424:2424 -p 2480:2480 --name orientdb_2424_$db joaodubas/orientdb
+docker logs orientdb_2424_$db | head -n 30
 
-docker run -d -p 8091:8091 --name couchbase_8091_$bench couchbase/server
-docker logs couchbase_8091_$bench | head -n 30
+docker run -d -p 8091:8091 --name couchbase_8091_$db couchbase/server
+docker logs couchbase_8091_$db | head -n 30
 
-docker run -d -p 7000:7000 --name cassandra_7000_$bench cassandra
-docker logs cassandra_7000_$bench | head -n 30
+docker run -d -p 7000:7000 --name cassandra_7000_$db cassandra
+docker logs cassandra_7000_$db | head -n 30
 
-docker run -d -p 8983:8983 --name solr_8983_$bench makuk66/docker-solr
-docker logs solr_8983_$bench | head -n 30
+docker run -d -p 8983:8983 --name solr_8983_$db makuk66/docker-solr
+docker logs solr_8983_$db | head -n 30
 
-docker run -d -p 8080:8080 --name hadoop_8080_$bench sequenceiq/hadoop-docker
-docker logs hadoop_8080_$bench | head -n 30
+docker run -d -p 8080:8080 --name hadoop_8080_$db sequenceiq/hadoop-docker
+docker logs hadoop_8080_$db | head -n 30
 
-docker run -d -p 5601:5601 --name kibana_5601_$bench --link elastic:elasticsearch kibana
-docker logs kibana_5601_$bench | head -n 30
+docker run -d -p 5601:5601 --name kibana_5601_$db --link elastic:elasticsearch kibana
+docker logs kibana_5601_$db | head -n 30
 
 #docker pull devdb/kibana
