221f4d13f7e6d4ce2813fc492afff0eb368ade9d
Marcelo M. Fleury <marcelomf@gmail.com>
2015-06-27 10:03:38 -0300

    Several improvements in automation

--- db/benchmark.sh
+++ db/benchmark.sh
@@ -5,31 +5,10 @@ uri_file="uri.txt"
 
 while IFS='' read -r uri || [[ -n $uri ]]
 do
-  log=$(echo $uri | sed -e "s/:\/\//_/g" -e "s/:/_/g" -e "s/\//_/g" -e "s/@/_/g")
-  driver=$(echo $log | cut -d'_' -f1)
-  port=$(echo $log | rev | cut -d'_' -f2 | rev)
-  if [[ "$port" =~ ^[0-9]+$ ]]
-  then
-    db=$(echo $log | rev | cut -d'_' -f1 | rev)
-  else
-    port=$(echo $log | rev | cut -d'_' -f1 | rev)
-    db="null"
-  fi
-
-  if [ "$db" = "null" ]
-  then
-    if [ "$port" = "$driver" ]
-    then
-      container_name="$driver"
-    else
-      container_name="$driver"_"$port"
-    fi
-  else
-    container_name="$driver"_"$port"_"$db"
-  fi
-  echo "BEGIN: $container_name - $log - $uri_file - $(date)"
+  container_name=$(./uri2name.sh $uri)
+  echo "BEGIN: $container_name - $uri_file - $(date)"
   docker start $container_name > /dev/null
   time node import_$import_type.js $uri $file_dir
   docker stop $container_name > /dev/null
-  echo "ENDED: $container_name - $log - $uri_file - $(date)"
+  echo "ENDED: $container_name - $uri_file - $(date)"
 done < "$uri_file"
--- db/build_env.sh
+++ db/build_env.sh
@@ -1,55 +1,71 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_0.12 | bash -
-
-apt-get update -y
-apt-get install -y libmysqlclient-dev mongodb-clients mysql-client nodejs postgresql-client postgresql-server-dev-9.3 curl docker.io sudo
 
 #curl -sL https://deb.nodesource.com/setup_iojs_1.x | sudo bash -
 #apt-get install -y iojs
 
+curl -sL https://deb.nodesource.com/setup_0.12 | bash -
+apt-get update -y
+apt-get install -y libmysqlclient-dev mongodb-clients mysql-client nodejs postgresql-client postgresql-server-dev-9.3 curl docker.io sudo
 sudo npm update -g
 sudo npm install -g bookshelf caminte elasticsearch mongodb mongoose mysql orm pg pg-hstore pg-native sax sequelize sequelize-cli sqlite3 tedious underscore-cli sql-query loopback-datasource-juggler loopback-connector-mongodb ya-csv fast-csv csv-streamify csv csv-parse stream-transform
 
 db="bench"
+uri_file="uri.txt"
+images=$(mktemp)
+cat << EOF > $images
+5432:postgresql:tutum/postgresql
+3306:mysql:tutum/mysql
+3306:mariadb:tutum/mariadb
+3306:percona:percona
+27017:mongodb:tutum/mongodb
+9200:elasticsearch:tutum/elasticsearch
+5984:couchdb:tutum/couchdb
+8098:riak:tutum/riak
+2480:orientdb:joaodubas/orientdb
+8091:couchbase:couchbase/server
+7000:cassandra:cassandra
+8983:solr:makuk66/docker-solr
+8080:hadoop:sequenceiq/hadoop-docker
+EOF
 
-docker run -d -p 5432:5432 --name postgres_5432_$db tutum/postgresql
-docker logs postgres_5432_$db | head -n 30
+while IFS='' read -r uri || [[ -n $uri ]]
+do
+  container_name=$(./uri2name.sh $uri)
+  driver=$(echo $container_name | cut -d'_' -f1)
+  host_port=$(echo $container_name | cut -d'_' -f2)
+  container_port=$(cat $images | grep $driver | cut -d':' -f1)
+  image=$(cat $images | grep $driver | cut -d':' -f3)
+  
+  echo -e "----- UP CONTAINER: $container_name -----"
+  if [ "$driver" = "mongodb"  ]
+  then
+    docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no --name mongodb_27017_$db tutum/mongodb
+    sleep 3
+    docker logs mongodb_27017_$db | head -n 30
+    continue
+  fi
 
-docker run -d -p 3306:3306 --name mysql_3306_$db tutum/mysql
-docker logs mysql_3306_$db | head -n 30
+  if [ "$driver" = "riak"  ]
+  then
+    docker run -d -p 8087:8087 -p 8098:8098 --name riak_8087_$db tutum/riak
+    sleep 3
+    docker logs riak_8087_$db | head -n 30
+    continue
+  fi
 
-docker run -d -p 3366:3306 --name mariadb_3366_$db tutum/mariadb
-docker logs mariadb_3366_$db | head -n 30
+  if [ "$driver" = "orientdb"  ]
+  then
+    docker run -d -p 2424:2424 -p 2480:2480 --name orientdb_2424_$db joaodubas/orientdb
+    sleep 3
+    docker logs orientdb_2424_$db | head -n 30
+    continue
+  fi
 
-docker run -d -p 3300:3306 --name percona_3300_$db percona
-docker logs percona_3300_$db | head -n 30
+  docker run -d -p $host_port:$container_port --name $container_name $image
+  sleep 3
+  docker logs $container_name | head -n 30
 
-docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no --name mongodb_27017_$db tutum/mongodb
-docker logs mongodb_27017_$db | grep "mongo admin"
-
-docker run -d -p 9200:9200 --name elastic_9200_$db tutum/elasticsearch
-docker logs elastic_9200_$db | head -n 30
-
-docker run -d -p 5984:5984 --name couchdb_5984_$db tutum/couchdb
-docker logs couchdb_5984_$db | head -n 30
-
-docker run -d -p 8087:8087 -p 8098:8098 --name riak_8087_$db tutum/riak
-docker logs riak_8087_$db | head -n 30
-
-docker run -d -p 2424:2424 -p 2480:2480 --name orientdb_2424_$db joaodubas/orientdb
-docker logs orientdb_2424_$db | head -n 30
-
-docker run -d -p 8091:8091 --name couchbase_8091_$db couchbase/server
-docker logs couchbase_8091_$db | head -n 30
-
-docker run -d -p 7000:7000 --name cassandra_7000_$db cassandra
-docker logs cassandra_7000_$db | head -n 30
-
-docker run -d -p 8983:8983 --name solr_8983_$db makuk66/docker-solr
-docker logs solr_8983_$db | head -n 30
-
-docker run -d -p 8080:8080 --name hadoop_8080_$db sequenceiq/hadoop-docker
-docker logs hadoop_8080_$db | head -n 30
+done < "$uri_file"
 
 docker run -d -p 5601:5601 --name kibana_5601_$db --link elastic:elasticsearch kibana
 docker logs kibana_5601_$db | head -n 30
