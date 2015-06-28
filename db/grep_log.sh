0ae3cdd58fa641c57b4097e21e71e2f6b3ce9528
Marcelo M. Fleury <marcelomf@gmail.com>
2015-06-27 23:06:09 -0300

    Improve grep_log.sh

--- db/grep_log.sh
+++ db/grep_log.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-egrep -v -e "xml|csv" -e "  " $1 | xargs echo | sed \
-    -e "s/BEGIN: /\n/g" \
-    -e "s/ENDED: /\n/g" \
+log=$1
+egrep -v -i \
+    -e "xml|csv" \
+    -e "^erro" \
+    -e "ENDED:.*-.*-.*" -e "  " $log | \
+    sed -e "s/-.*-.*//g" | \
+    xargs echo | sed \
+    -e "s/BEGIN:/\n/g" \
     -e "s/TOTAL_FILES: //g" \
     -e "s/TOTAL_FILES_ENDED: //g" \
     -e "s/TOTAL_FILES_ERROR: //g" \
     -e "s/user //g" \
     -e "s/real //g" \
     -e "s/sys //g" \
-    | sed -e "s/ /,/g"
+    -e "s/ /,/g" \
+    | sed -e "s/^,//g" -e "s/,$//g"
